f0f048cda94dc1a0e9389cf505700d01d42c79e8
Merai <intern.rj@merai.co>
2025-04-16 22:48:05 +0530

    feat (Interceptor): Excustion time

---
+++ src/execution-time.interceptor.ts
@@ -0,0 +1,23 @@
+// src/url/execution-time.interceptor.ts
+
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class ExecutionTimeInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const now = Date.now();
+    return next.handle().pipe(
+      tap(() => {
+        const executionTime = Date.now() - now;
+        console.log(`Execution time: ${executionTime}ms`);
+      }),
+    );
+  }
+}
